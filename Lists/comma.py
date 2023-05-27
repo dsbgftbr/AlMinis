ebfb6476214a63b2ec4992217eb08193d15632e3
jagpo <jag.poline@gmail.com>
2023-05-27 23:23:56 +0800

    Rewrote comma.py

--- Lists/comma.py
+++ Lists/comma.py
@@ -4,13 +4,14 @@ pets = ["fish", "squid", "cats"]
 
 def comma_join(strings):
     listlength = len(strings)
+
     if not (strings):
         return
     elif listlength == 1:
         return strings[0]
     else:
         mid = ("," if listlength > 3 else "") + " and "
-        return f"{', '.join(strings[:-1])}{mid}{strings[-1]}"
+        return ", ".join(strings[:-1]) + mid + strings[-1]
 
 
 print(comma_join(spam))
